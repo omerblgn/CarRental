04c7768e7e2111cc0fc74df08e90f5c45dba046d
omerblgn <omerbilgen01@gmail.com>
2024-03-21 14:23:42 +0300

    Create Core layer & Make classes in DataAccess layer generic

--- ConsoleUI/Program.cs
+++ ConsoleUI/Program.cs
@@ -3,9 +3,38 @@ using DataAccess.Concrete.EntityFramework;
 using DataAccess.Concrete.InMemory;
 using Entities.Concrete;
 
-CarManager carManager = new CarManager(new EfCarDal());
+//CarTest();
 
-foreach (var car in carManager.GetCarsByColorId(1))
+//BrandTest();
+
+ColorTest();
+
+static void CarTest()
 {
-    Console.WriteLine(car.Description);
+    CarManager carManager = new CarManager(new EfCarDal());
+
+    foreach (var car in carManager.GetCarsByColorId(1))
+    {
+        Console.WriteLine(car.Description);
+    }
+}
+
+static void BrandTest()
+{
+    BrandManager brandManager = new BrandManager(new EfBrandDal());
+
+    foreach (var brand in brandManager.GetAll())
+    {
+        Console.WriteLine(brand.Name);
+    }
+}
+
+static void ColorTest()
+{
+    ColorManager colorManager = new ColorManager(new EfColorDal());
+
+    foreach (var color in colorManager.GetAll())
+    {
+        Console.WriteLine(color.Name);
+    }
 }
--- DataAccess/Concrete/EntityFramework/EfCarDal.cs
+++ DataAccess/Concrete/EntityFramework/EfCarDal.cs
@@ -1,63 +1,15 @@
-﻿using DataAccess.Abstract;
+﻿using Core.DataAccess.EntityFramework;
+using DataAccess.Abstract;
 using Entities.Concrete;
-using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Linq.Expressions;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace DataAccess.Concrete.EntityFramework
 {
-    public class EfCarDal : ICarDal
+    public class EfCarDal : EfEntityRepositoryBase<Car, CarRentalContext>, ICarDal
     {
-        public void Add(Car entity)
-        {
-            using (CarRentalContext context = new CarRentalContext())
-            {
-                var addedCar = context.Entry(entity);
-                addedCar.State = EntityState.Added;
-                context.SaveChanges();
-            }
-        }
-
-        public void Delete(Car entity)
-        {
-            using (CarRentalContext context = new CarRentalContext())
-            {
-                var deletedCar = context.Entry(entity);
-                deletedCar.State = EntityState.Deleted;
-                context.SaveChanges();
-            }
-        }
-
-        public Car Get(Expression<Func<Car, bool>> filter)
-        {
-            using (CarRentalContext context = new CarRentalContext())
-            {
-                return context.Set<Car>().SingleOrDefault(filter);
-            }
-        }
-
-        public List<Car> GetAll(Expression<Func<Car, bool>> filter = null)
-        {
-            using (CarRentalContext context = new CarRentalContext())
-            {
-                return filter == null
-                    ? context.Set<Car>().ToList()
-                    : context.Set<Car>().Where(filter).ToList();
-            }
-        }
-
-        public void Update(Car entity)
-        {
-            using (CarRentalContext context = new CarRentalContext())
-            {
-                var updatedCar = context.Entry(entity);
-                updatedCar.State = EntityState.Modified;
-                context.SaveChanges();
-            }
-        }
     }
 }
